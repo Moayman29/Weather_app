4fc5d2d951ddbe5725f2dec431a557b363af3ae4
Mohamed Ayman <m.ayman_299563@yahoo.com>
2023-06-13 22:28:32 +0300

    first commit

---
+++ lib/services/weather.dart
@@ -0,0 +1,70 @@
+import 'package:clima_application/services/location.dart';
+import 'package:clima_application/services/networking.dart';
+
+const apiKey = '4e3da7806c770ec00b60040fe477f82f';
+
+const openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+class WeatherModel {
+  double? latitude;
+  double? longitude;
+
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+      Uri.parse('$openWeatherMapUrl?q=$cityName&appid=$apiKey'),
+    );
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+      Uri.parse(
+          '$openWeatherMapUrl?lat=$latitude&lon=$longitude&appid=$apiKey'),
+    );
+
+    print('latitude =' + latitude.toString());
+    print('longitude =' + longitude.toString());
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
